6f15b52519b7bcaed3677afc858b6ef8df678b3e
Kristian Vårdal <kristian.vardal@gmail.com>
2014-01-25 00:37:50 +0100

    new level and a lot of testing

--- Assets/Scipts/Boost.cs
+++ Assets/Scipts/Boost.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class script : MonoBehaviour {
-
+public class Boost : MonoBehaviour {
+	
 	public GameObject player;
 	private float speed;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		rigidbody.AddForce (Vector3.forward * Time.deltaTime * speed);
-	
+		}
+
+	void OnCollisionEnter (Collision c) {
+		Debug.Log ("collided with: " + c.collider.gameObject);
+		c.gameObject.rigidbody.transform.Translate(Vector3.forward * 5);
 	}
 }
+
