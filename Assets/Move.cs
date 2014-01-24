9116804c2af65d85afa260dac8ae093c44524fd5
Baste Nesse Buanes <baste.buanes@student.uib.no>
2014-01-24 21:22:01 +0100

    initial commit

---
+++ Assets/Move.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+		private LolScript l;
+		public float speed;
+
+		// Use this for initialization
+		void Start ()
+		{
+				l = GetComponent<LolScript> ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				rigidbody.AddForce (1f, 0, 0);
+		}
+}
