fcd9b76cb355754ae75152b4b6297826dcb3ce93
Kristian Vårdal <kristian.vardal@gmail.com>
2014-01-25 11:40:14 +0100

    some stuff in hellfire level added

--- Assets/Scipts/Boost.cs
+++ Assets/Scipts/Boost.cs
@@ -2,21 +2,21 @@
 using System.Collections;
 
 public class Boost : MonoBehaviour {
-	
+
 	public GameObject player;
 	private float speed;
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		}
-
-	void OnCollisionEnter (Collision c) {
-		Debug.Log ("collided with: " + c.collider.gameObject);
-		c.gameObject.rigidbody.transform.Translate(Vector3.forward * 5);
+		//rigidbody.AddForce (Vector3.forward * Time.deltaTime * speed);
 	}
-}
 
+	void OnTriggerEnter(Collider c) {
+		c.gameObject.rigidbody.AddForce (200f,200f,-600f);
+
+}
+}
--- Assets/Scipts/RepeatTiles.cs
+++ Assets/Scipts/RepeatTiles.cs
@@ -1,10 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class script : MonoBehaviour {
+public class RepeatTiles : MonoBehaviour {
 
-	public GameObject player;
-	private float speed;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,7 +10,6 @@ public class script : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		rigidbody.AddForce (Vector3.forward * Time.deltaTime * speed);
 	
 	}
 }
