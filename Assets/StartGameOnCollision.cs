009ee4ca53a499cc08743f2ea580ee0733c8e621
Baste Nesse Buanes <baste.buanes@student.uib.no>
2014-01-25 21:55:06 +0100

    first stage done scripted, rdy for clutter

--- Assets/StartGameOnCollision.cs
+++ Assets/StartGameOnCollision.cs
@@ -5,14 +5,18 @@ public class StartGameOnCollision : MonoBehaviour {
 
 	public Player player;
 	public Transform body;
+	private bool hasBeenCollidedWith = false;
 
 	void OnTriggerEnter (Collider c) {
-		c.gameObject.rigidbody.velocity = Vector3.zero;
-		c.gameObject.rigidbody.useGravity = false;
-		c.gameObject.rigidbody.isKinematic = true;
-		c.gameObject.rigidbody.freezeRotation = true;
-
-		body.gameObject.SetActive (false);
-		player.gameObject.SetActive (true);
+		if (!hasBeenCollidedWith) {
+			c.gameObject.rigidbody.velocity = Vector3.zero;
+			c.gameObject.rigidbody.useGravity = false;
+			c.gameObject.rigidbody.isKinematic = true;
+			c.gameObject.rigidbody.freezeRotation = true;
+			
+			body.gameObject.SetActive (false);
+			player.gameObject.SetActive (true);
+			hasBeenCollidedWith = true;
+		}
 	}
 }
