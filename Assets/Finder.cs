4964ba9d76b34ea2234a7e21f64f44d7e57d47b0
Kristian Vårdal <kristian.vardal@gmail.com>
2014-01-25 21:30:14 +0100

    helllevel works

---
+++ Assets/Finder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log ("cameras: " + Object.FindObjectsOfType<HellFireCamera> ());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
--- Assets/Scipts/Boost.cs
+++ Assets/Scipts/Boost.cs
@@ -4,17 +4,25 @@ using System.Collections;
 public class Boost : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
 		//rigidbody.AddForce (Vector3.forward * Time.deltaTime * speed);
 	}
 
-	void OnTriggerEnter(Collider c) {
-		c.gameObject.rigidbody.AddForce (0f,200f,-600f);
+	void OnTriggerEnter (Collider c)
+	{	
+		Transform t = c.transform.parent;
+		if (t != null) {
+			Rigidbody r = c.transform.parent.gameObject.rigidbody;
+			if (r != null)
+				r.AddForce (0f, 200f, -600f);
+		}
 	}
 }
