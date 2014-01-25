5216bb8e67c69b9a7ccae149a62994d907c75bc7
Baste Nesse Buanes <baste.buanes@student.uib.no>
2014-01-25 23:22:27 +0100

    new level oh yeah

---
+++ Assets/SpinScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0f, 1f, 0f);
+	}
+}
