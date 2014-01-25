388dbe683938cea4bd52e58ef49589a2e54a0edf
Baste Nesse Buanes <baste.buanes@student.uib.no>
2014-01-25 19:31:06 +0100

    implemented winning level

---
+++ Assets/WinSphere.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 
+/// </summary>
+public class WinSphere : MonoBehaviour {
+
+	public GUIText winText;
+	public string nextLevel;
+	private bool hasWon = false;
+	private float winTime;
+
+	void Update () {
+		if (hasWon) {
+			if (Time.time - winTime > 5f) {
+				Application.LoadLevel (nextLevel);
+			}
+		}
+	}
+
+
+	void OnTriggerEnter (Collider c) {
+		winText.enabled = true;
+		hasWon = true;
+		winTime = Time.time;
+	}
+}
