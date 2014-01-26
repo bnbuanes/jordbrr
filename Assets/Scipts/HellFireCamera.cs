33fe84bed514854a7c9f37dab1c0a89a9bd5ad12
Baste Nesse Buanes <baste.buanes@student.uib.no>
2014-01-26 11:48:26 +0100

    done before the rest shows up yo

--- Assets/Scipts/HellFireCamera.cs
+++ Assets/Scipts/HellFireCamera.cs
@@ -2,53 +2,43 @@ using UnityEngine;
 using System.Collections;
 
 public class HellFireCamera : MonoBehaviour {	
-	public Camera mainCamera;
 
 	public GameObject player;
 	public GameObject EndSpawn;
-	private Vector3 endSpawnVector;
 	private bool hasMoved;
 	private bool hasCameraRotated;
 
 	// Use this for initialization
-	void Start ()
-	{
-		//mainCamera = Camera.main;
+	void Start () {
 		hasMoved = false;
 		hasCameraRotated = false;
 	}
 	
 	// Update is called once per frame
-	void Update ()
-	{
-		float distance = Vector3.Distance (player.transform.position, mainCamera.transform.position);
+	void Update () {
+		float distance = Vector3.Distance (player.transform.position, transform.position);
 
 		if (distance < 20 && hasMoved.Equals (false)) {
 			raiseCamera (distance);
 		} else { 
 			lowerCamera (distance);
 			hasMoved = true;
-			//transform.position = new Vector3 (EndSpawn.transform.position.x, 5f, EndSpawn.transform.position.z);
 		}
 	}
 		
-	void raiseCamera (float distance)
-	{
+	void raiseCamera (float distance) {
 		float y = 1f + 0.3f * distance;
 
-		transform.position = new Vector3 (transform.position.x, y, transform.position.y);
-
+		transform.position = new Vector3 (transform.position.x, y, transform.position.z);
 	}
 
-	void lowerCamera (float distance)
-	{	
-		endSpawnVector = EndSpawn.transform.position;
-		float y = 0.5f + (distance - 1.3f) / distance;
-
+	void lowerCamera (float distance) {	
+		float y = 1f + (distance) / 5f;
 
 		transform.position = new Vector3 (EndSpawn.transform.position.x, y, EndSpawn.transform.position.z);
 		if (hasCameraRotated.Equals (false)) {
 			transform.RotateAround (transform.position, transform.up, 180f);
+			transform.RotateAround (transform.position, transform.right, 40f);
 			hasCameraRotated = true;
 		}
 	}
---
+++ Assets/Scipts/PlayOnImpact.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Plays a sound whenever hit by a Collision or a Collider - can be used on trigger and nontrigger colliders.
+/// </summary>
+public class PlayOnImpact : MonoBehaviour {
+
+	private AudioSource sound;
+	/// <summary>
+	/// Set to true if the script should only play a sound on the first impact
+	/// </summary>
+	public bool PlayOnlyOnce;
+	private bool shouldPlay = true;
+
+	void Start () {
+		sound = GetComponent<AudioSource> ();
+	}
+
+	void OnCollisionEnter (Collision c) {
+		PlaySound ();
+	}
+
+	void OnTriggerEnter (Collider c) {
+		PlaySound ();
+	}
+
+	void PlaySound () {
+		if (shouldPlay) {
+			sound.Play ();
+			if (PlayOnlyOnce)
+				shouldPlay = false;
+		}
+	}
+}
